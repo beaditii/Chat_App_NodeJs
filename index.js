b720267ec78ad4cfd99562620943d6581b83e69a
Aditi Kumari <aditisingh49893@gmail.com>
2024-02-26 00:16:00 +0530

    Added codes for two way interaction

--- index.js
+++ index.js
@@ -11,15 +11,27 @@ app.use('/',express.static(__dirname + '/public'));
  io.on('connection',(socket)=>{
     console.log('a user connected');
 
-      socket.on('from_client',()=>{
-        console.log('event coming from client');
+      socket.on('msg_send',(data)=>{
+        console.log(data);
+        // io.emit('msg_rcvd',data);
+        //socket.broadcast.emit('msg_rcvd',data);
+        socket.emit('msg_rcvd',data);
       })
-      
-    setInterval(()=>{
-        socket.emit('from_server');
-    },5000);
+
+    
  });
 
 server.listen(3000,()=>{
     console.log('Server started');
 });
+
+/**
+ * io.emit() is for all websocket connnection that exists with corresponsding server
+ * e.g. if any of browser chrome,internet,brave send message every browser in the room will receive message
+ * 
+ * socket.emit() is only for that particular client taht sends message will recieve it
+ * e.g. if chrome sends message then only chrome receive,remainig are not going to receive message
+ * 
+ * socket.broadcast.emit() then remainig browser that donot send message will recieve it
+ * e.g. if chrome sends then other than chrome all browser will receive
+ */
--- public/script.js
+++ public/script.js
@@ -1,13 +1,17 @@
 var socket=io();
 
 let btn=document.getElementById("btn");
+let inputMsg=document.getElementById("newmsg");
+let msgList=document.getElementById("msglist");
 
 btn.onclick=function exec(){
-    socket.emit('from_client');
+    socket.emit('msg_send',{
+        msg:inputMsg.value,
+    });
 }
 
-socket.on('from_server',()=>{
+socket.on('msg_rcvd',(data)=>{
     const div=document.createElement('div');
-    div.innerText='New event from server';
+    div.innerText=data.msg;
     document.body.appendChild(div);
 })
